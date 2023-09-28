c4b0e27d98357470a1730d47e9929f216a510837
5520123013-Zaenal <144973949+5520123013-Zaenal@users.noreply.github.com>
2023-09-28 19:50:08 +0800

    Update and rename README.md to Nomor 3.py
    Membuat program yang menghitung luas persegi panjang dengan menggunakan fungsi input.

---
+++ Nomor 3.py
@@ -0,0 +1,12 @@
+# bab1_Projek_python_nomor_3
+# Meminta input panjang dari pengguna
+panjang = float(input("Masukkan panjang persegi panjang: "))
+
+# Meminta input lebar dari pengguna
+lebar = float(input("Masukkan lebar persegi panjang: "))
+
+# Menghitung luas persegi panjang
+luas = panjang * lebar
+
+# Menampilkan hasil
+print("Luas persegi panjang adalah:", luas)
